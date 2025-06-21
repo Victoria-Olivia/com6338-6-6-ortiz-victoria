3d6a5dd995d8a43d72b349ef0f8d2a275a1624be
Victoria Ortiz <ortiz.victoria@ufl.edu>
2025-06-21 13:38:45 -0400

    Added hamburger menu open and close button

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 var hamburgerMenu = document.querySelector('.hamburger-menu');
 var hamburgerBtn = document.querySelector('.hamburger-btn');
+hamburgerBtn.addEventListener('click', function() {
+    var menuIsOpen = hamburgerMenu.classList.contains('show-menu');
+    if (menuIsOpen) {
+        hamburgerMenu.classList.remove('show-menu');
+        hamburgerBtn.setAttribute('aria-expanded', 'false');
+    } else {
+        hamburgerMenu.classList.add('show-menu');
+        hamburgerBtn.setAttribute('aria-expanded', 'true');
+    }
+});
